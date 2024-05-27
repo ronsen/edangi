1b80d38840e00f1d99b3bd5548ac41fc1390d45e
Ronsen <ronsen@startrek.or.id>
2024-05-27 13:32:40 +0700

    fix: ayat acak

--- src/lib/ayat-acak.js
+++ src/lib/ayat-acak.js
@@ -2,21 +2,21 @@ import axios from "axios";
 import * as cheerio from 'cheerio';
 
 export const ayatAcak = async () => {
-	const url = 'https://www.bible.com/id/verse-of-the-day';
-	const response = await axios.get(url);
-	const body = await response.data;
-	const status = response.status;
+  const url = 'https://www.bible.com/id/verse-of-the-day';
+  const response = await axios.get(url);
+  const body = await response.data;
+  const status = response.status;
 
-	const data = {};
+  const data = {};
 
-	if (status == 200) {
-		const $ = cheerio.load(body);
-		const verse = $(".pis-2.plb-1.pli-1.border-black.border-l-large.border.mbs-3 > .text-gray-25").text();
-		const content = $(".pis-2.plb-1.pli-1.border-black.border-l-large.border.mbs-3 > .text-gray-50").text();
+  if (status == 200) {
+    const $ = cheerio.load(body);
+    const verse = $("#__next > div > main > div > div > div > div:nth-child(1) > div > a:nth-child(2) > p").text();
+    const content = $("#__next > div > main > div > div > div > div:nth-child(1) > div > a:nth-child(1)").html().replace(/[\n\r]+/g, ' ');;
 
-		data.verse = verse;
-		data.content = content;
-	}
+    data.verse = verse;
+    data.content = content;
+  }
 
-	return { status, data };
+  return { status, data };
 }
